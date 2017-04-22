574438de3bdea494fb3230e1786e0629f8c53f3f
Gordon Evans <gjrevans@gmail.com>
2017-04-22 18:20:47 -0400

    Put in some dummy data for testing.

--- routes/books.js
+++ routes/books.js
@@ -10,6 +10,21 @@ BookRoutes.prototype.index = function(req, res) {
         summary: "This is a really great book, that everyone will like.",
         image: "https://images-na.ssl-images-amazon.com/images/I/61aY1f%2Btn6L._SY344_BO1,204,203,200_.jpg",
         _id: "1234"
+    },{
+        title: "Managing Humans",
+        summary: "This is a really great book, that everyone will like.",
+        image: "https://images-na.ssl-images-amazon.com/images/I/61aY1f%2Btn6L._SY344_BO1,204,203,200_.jpg",
+        _id: "1234"
+    },{
+        title: "Managing Humans",
+        summary: "This is a really great book, that everyone will like.",
+        image: "https://images-na.ssl-images-amazon.com/images/I/61aY1f%2Btn6L._SY344_BO1,204,203,200_.jpg",
+        _id: "1234"
+    },{
+        title: "Managing Humans",
+        summary: "This is a really great book, that everyone will like.",
+        image: "https://images-na.ssl-images-amazon.com/images/I/61aY1f%2Btn6L._SY344_BO1,204,203,200_.jpg",
+        _id: "1234"
     }]
     res.render('books/index.html', {
         breadcrumbs: req.breadcrumbs(),
