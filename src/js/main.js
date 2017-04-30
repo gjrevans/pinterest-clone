995bcb07aed4150eca117722cd449f2a61b46f0b
Gordon Evans <gjrevans@gmail.com>
2017-04-30 15:38:06 -0400

    Catch broken images.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
-$(document).ready(function(){});
+// Catch broken images before the dom loads
+function imgError(image) {
+	image.onerror = "";
+	image.src = "https://bytesizemoments.com/wp-content/uploads/2014/04/placeholder.png";
+	return true;
+}
